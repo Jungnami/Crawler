3775d24383b4ed287388906d69c8baace8b0f871
강수진 <rkdthd0403@gmail.com>
2019-01-31 20:10:03 +0900

    함수화 및 기사 안 내용 크롤링

--- crawling/article_crawler.py
+++ crawling/article_crawler.py
@@ -5,56 +5,87 @@ import datetime
 from secretModule import dbconfig
 from collections import namedtuple
 
-# 현재 사이트에 있는 최신 순위 기사들 crawling
-todayDate = datetime.datetime.today().strftime('%Y%m%d')
-req = requests.get('https://news.naver.com/main/ranking/popularDay.nhn?rankingType=popular_day&sectionId=100&date='+todayDate)
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
-crawlingData = []
 
-for i in range(1, 31):
-    fields = 'title link img_src office registe_date update_date ranking'
-    crawlingTuple = namedtuple('crawlingTuple', fields)
-    article = soup.find("li", {"class": "ranking_item is_num"+str(i)})
-    img_src = ''
-    if not (article.img is None):
-        img_src = article.img.get('src')
-    crawlingData.append(crawlingTuple(article.a.get('title'), article.a.get('href'), img_src, article.find('div', {"class": "ranking_office"}).text, datetime.datetime.now(), datetime.datetime.now(), i))
+# soup 만들어내는 함수
+def get_soup(url):
+    return BeautifulSoup(requests.get(url).text, 'html.parser')
 
-# DB에 있는 최신 기사 fetch
+
+# 크롤링 함수
+def get_crawling_data():
+    todayDate = datetime.datetime.today().strftime('%Y%m%d')
+    currentArticleUrl = 'https://news.naver.com/main/ranking/popularDay.nhn?rankingType=popular_day&sectionId=100&date='+todayDate
+    articleListSoup = get_soup(currentArticleUrl)
+    crawling_data = []
+    articleList = articleListSoup.find("ol", {"class": "ranking_list"})
+    for i in range(1, 31):
+        article = articleList.find("li", {"class": "ranking_item is_num" + str(i)})
+        title = article.a.get('title')
+        detailUrl = str("https://news.naver.com" + article.a.get('href'))
+        office = article.find('div', {"class": "ranking_office"}).text
+        ranking = i
+        img_src = article.img.get('src') if not (article.img is None) else ''
+
+        # 기사 상세보기 들어가서 찾는 정보
+        articleDetailSoup = get_soup(detailUrl)
+        realArticleUrl = articleDetailSoup.find("div", {"class": "sponsor"}).a.get('href')
+
+        articleDetailBody = articleDetailSoup.find('div', {"id": "articleBodyContents"})
+        # TODO 디비에 넣기
+        print articleDetailBody.text
+
+        fields = 'title link img_src office registe_date update_date ranking'
+        crawlingTuple = namedtuple('crawlingTuple', fields)
+        crawling_data.append(
+            crawlingTuple(title, realArticleUrl, img_src, office, datetime.datetime.now(), datetime.datetime.now(),
+                          ranking))
+    return crawling_data
+
+
+# 디비 페치 함수
+def get_fetched_data(cursor):
+    sql = "select t.id, t.link from crawling.article t " \
+          "inner join (" \
+          "select ranking, max(update_date) as MaxDate, max(id) as MaxId from crawling.article group by ranking" \
+          ") tm on t.ranking = tm.ranking and t.update_date = tm.MaxDate and  t.id = tm.MaxId order by t.ranking;"
+
+    cursor.execute(sql)
+
+    fetchedTuple = namedtuple('fetchedTuple', 'id link')
+    fetched_data = []
+    for result in cursor.fetchall():
+        result = fetchedTuple(result[0], result[1])
+        fetched_data.append(result)
+    return fetched_data
+
+
+def get_intersection_id(x):
+    if x.link in list(map(lambda y: y.link, crawlingData)):
+        return {x.link: x.id}
+
+
 conn = dbconfig.dbConnect()
 curs = conn.cursor()
-# TODO ranking 이 최신인거 30개 가져와야함. ranking 1-30까지 최근에 업데이트 된것들 골라야함
-# sql = "SELECT id, link FROM article WHERE update_date=(SELECT MAX(update_date) FROM article) ORDER BY ranking;"
-# curs.execute(sql)
 
-sql = "select * from article where ranking = '1' and update_date = max(update_date);"
-curs.execute(sql)
-result = curs.fetchall()
-#
-# fetchedTuple = namedtuple('fetchedTuple', 'id link')
-# fetchedData = []
-# for result in curs.fetchall():
-#     result = fetchedTuple(result[0], result[1])
-#     fetchedData.append(result)
-#
-# # fetch 를 통해 가져온것과 크롤링 해온 것 중복 검사 -> fetchedData vs crawlingData의 link 이용
-# # crawlingData 는 title, link, img_src, registe_date, update_date, ranking
-# # fetchedDate 는 id, link
-#
-# intersection = set(list(map(lambda x: x.link, crawlingData))) & set(list(map(lambda x: x.link, fetchedData)))
-#
-# #TODO id 로 where 절 걸 방법 찾기
-# for item in crawlingData:
-#     if item.link in intersection:
-#         print "update"
-#         sql = "UPDATE article SET title = %s, link = %s, img_src = %s, office = %s, ranking = %s, update_date = %s WHERE link = %s"
-#         val = (item.title, item.link, item.img_src, item.office, item.ranking, datetime.datetime.now(), item.link)
-#         curs.execute(sql, val)
-#         conn.commit()
-#     else:
-#         print "insert"
-#         sql = "INSERT INTO article (title, link, img_src, office, registe_date, update_date, ranking) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-#         curs.execute(sql, item)
-#         conn.commit()
+crawlingData = get_crawling_data()
+fetchedData = get_fetched_data(curs)
+
+# fetch 를 통해 가져온것과 크롤링 해온 것 중복 검사 -> fetchedData vs crawlingData의 link 이용.
+# link, id keyValue 쌍 이용
+dictList = list(filter(lambda x: x is not None, list(map(get_intersection_id, fetchedData))))
+idLinkDic = {key: value for d in dictList for key, value in d.items()}
+
+for item in crawlingData:
+    if item.link in idLinkDic.keys():
+        articleId = idLinkDic[item.link]
+        print "update"
+        sql = "UPDATE article SET title = %s, link = %s, img_src = %s, office = %s, ranking = %s, update_date = %s WHERE id = %s"
+        val = (item.title, item.link, item.img_src, item.office, item.ranking, datetime.datetime.now(), articleId)
+        curs.execute(sql, val)
+        conn.commit()
+    else:
+        print "insert"
+        sql = "INSERT INTO article (title, link, img_src, office, registe_date, update_date, ranking) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        curs.execute(sql, item)
+        conn.commit()
 conn.close()
